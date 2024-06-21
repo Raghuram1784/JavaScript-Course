f84088c21de34e31d3420aadd0692e5ff03817ae
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-21 04:06:42 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Ram"
+let age = 19
+let isLoggedin = false
+let state
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedin);
+
