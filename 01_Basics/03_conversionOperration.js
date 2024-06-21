4c314df8925399002ac856ed79e07c96fccd92c4
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-21 04:51:36 +0000

    DataType Conversions

---
+++ 01_Basics/03_conversionOperration.js
@@ -0,0 +1,13 @@
+let score = "456"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+
+let bool = true
+console.log(typeof bool);
+let boolNum = Number(bool)
+console.log(typeof boolNum);
+
