094fbdc5a239404eb82b0e6e78904bb11afd38a1
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-21 04:06:14 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId = 8765534
+let accName = "Ram@gmail.com"
+var accPassword = "12345"
+accCity = "Hyd"
+let accState 
+
+// accId = 23434
+
+accName = "Raghu@gmail.com"
+accPassword = "98cmcjm"
+accCity = "mumbai"
+
+console.log(accId);
+
+console.table([accId,accName,accPassword,accCity,accState])
