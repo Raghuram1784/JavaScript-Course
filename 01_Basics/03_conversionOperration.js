f5a1a8c66ebdec951e613909b21fb7f24d362d66
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-21 05:04:20 +0000

    Data type conversion

--- 01_Basics/03_conversionOperration.js
+++ 01_Basics/03_conversionOperration.js
@@ -1,13 +1,37 @@
-let score = "456"
+// let score = "456"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueNumber = Number(score)
-console.log(typeof valueNumber);
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
 
-let bool = true
-console.log(typeof bool);
-let boolNum = Number(bool)
-console.log(typeof boolNum);
+// let bool = true
+// console.log(typeof bool);
+// let boolNum = Number(bool)
+// console.log(typeof boolNum);
 
+
+
+//******************************Operations*********************** 
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+
+
+// let s1 = "Raghu"
+// let s2 = " ram"
+// let s3 = s1+s2
+// console.log(s3);
+
+// console.log("1" + 2 + 4);
+// console.log(1 + 2 + "4");
+
+
+
+
+console.log(true);
+console.log(+true);
