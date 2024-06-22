f50756539f7a16e93ede99351fe7905e6c364ce3
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-22 05:36:05 +0000

    Dates In Js

---
+++ 01_Basics/07_datesInJs.js
@@ -0,0 +1,17 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(typeof myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+
+
+let createdDate = new Date(2024 , 5, 22 , 10 ,26)
+// console.log(createdDate.toDateString());
+console.log(createdDate.toLocaleString());
