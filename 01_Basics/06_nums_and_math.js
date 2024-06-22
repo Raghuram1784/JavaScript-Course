709d444ef78e9297b5ad31e4d5c804abf6f36b7c
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-22 04:31:21 +0000

    Math and Num

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+// const num = 200
+// console.log(num);
+
+// const pin = new Number(1234)
+// console.log(pin);
+
+// console.log(pin.toFixed(2));
+// console.log(pin.toString());
+// console.log(typeof pin);
+
+// const anotherNum = 125.678
+// console.log(anotherNum.toPrecision(3));
+// console.log(anotherNum.toPrecision(4));
+
+
+// const anotherNum1 = 100000000
+// console.log(anotherNum1.toLocaleString('en-IN'));
+
+
+
+// *****************************Maths**********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(98.45));
+// console.log(Math.round(98.56));
+// console.log(Math.ceil(98.32));
+// console.log(Math.floor(95.56));
+// console.log(Math.min(5,34,76,9,1,3));
+// console.log(Math.max(5,34,76,9,1,3));
+
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
+
+
