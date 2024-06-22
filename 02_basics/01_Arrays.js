880e8f3c16507eb5ece884d02425bb10920457a3
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-22 07:56:06 +0000

    Arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,35 @@
+const arr = [1,2,3,4,5]
+// console.log(arr[3]);
+
+// const heroes = ["Iron Man","SpiderMan","Thor","Hulk","Captian America"];
+// console.log(heroes[2]);
+
+//Array methods
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+// arr.unshift(0)
+// arr.shift()
+// console.log(arr.includes(8));
+// console.log(arr);
+
+// const newArr = arr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+
+//slice ,splice
+
+console.log("A ",arr);
+
+const arr1 = arr.slice(1, 3)
+console.log(arr1);
+console.log("B ",arr);
+
+const arr2 = arr.splice(1, 3)
+console.log("c ",arr);
+console.log(arr2);
+
