9eba0949533787747cb077f64d00f82371a6f9f2
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-23 10:46:03 +0000

    Objects

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,39 @@
+// const instaUser = new Object()
+const instaUser = {}
+
+instaUser.id ="12345"
+instaUser.name = "Sakshiiiiiii"
+instaUser.isLoogedIn = false
+
+// console.log(instaUser);
+
+const user = {
+    email : "Raghu@gmail.com",
+    fullName : {
+          userFullName : {
+            firstName : "Raghu",
+            lastName : "ram",
+          }
+    }
+}
+
+// console.log(user.fullName);
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+
+// const obj3 = {obj1 ,obj2}
+// const obj3 =  Object.assign(obj1 , obj2)
+// const obj3 =  Object.assign({} ,obj1 , obj2)
+const obj3 =  {...obj1, ...obj2}
+// console.log(obj3);
+
+
+
+
+
+console.log(instaUser)
+console.log(Object.keys(instaUser))
+console.log(Object.values(instaUser))
+console.log(Object.entries(instaUser))
+console.log(instaUser.hasOwnProperty("name"))
