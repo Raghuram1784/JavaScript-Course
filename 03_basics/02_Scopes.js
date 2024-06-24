cb07b38cafa1fd74190ba3c35220a35baa28f2b9
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-24 06:42:01 +0000

    Scopes

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,17 @@
+let a =300
+
+if(true){
+    let a = 10
+    const b =20
+    // var c = 30
+    console.log("Inside: ",a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
