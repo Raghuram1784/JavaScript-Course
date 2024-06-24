d27247e5367a556eae3add46b5f500d41321d110
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-24 07:10:47 +0000

    Scopes

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -4,14 +4,54 @@ if(true){
     let a = 10
     const b =20
     // var c = 30
-    console.log("Inside: ",a);
+    // console.log("Inside: ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+
+
+function one(){
+    const userName = "Ram"
+
+    function two(){
+        const website  = "GitHub"
+        console.log(userName);
+    }
+    // console.log(website);
+    two()
 }
+// one()
+
+
+
+if(true){
+    const userName = "Ram"
+    if (userName === "Ram") {
+        const website = " GitHub"
+        console.log(userName + website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+
+
+
+
+
+//************************************ Interesting ************************************
+
+console.log(addOne(8));
+function addOne(num){
+    return num+1
+}
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
+
