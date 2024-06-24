634cb43fcdbcc37db14d100eaeaaf41808f3ab13
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-24 05:59:56 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,47 @@ function loginUserMessage(userName){
 // console.log(loginUserMessage());
 // console.log(loginUserMessage(""));
 
-console.log(loginUserMessage("Ram"));
+// console.log(loginUserMessage("Ram"));
+
+
+
+function calculateCartPrice(...num1){ //...num1 ----->Rest Operator
+    return num1
+}
+
+// console.log(calculateCartPrice(2));
+// console.log(calculateCartPrice(200, 300, 400, 500));
+
+
+
+
+
+//************************************Functions with Objects****************************************
+
+const user = {
+    userName : "Ram",
+    price : 456
+}
+
+function handleObject(anyObject){
+    console.log(`UserName is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)   or
+handleObject({
+    userName : "Sakshi",
+    price : 999
+})   
+
+
+
+
+//************************************Functions with Arrays****************************************
+const arr = [100, 200, 300 , 900]
+
+function returnSecondValu(getArr){
+    return getArr[1]
+}
+
+// console.log(returnSecondValu(arr));
+console.log(returnSecondValu([100, 200, 300, 400]));
