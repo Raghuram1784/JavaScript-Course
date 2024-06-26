96a0b1e091e28a11df3679875365c3f3433bff13
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-26 03:44:00 +0000

    while & do-while

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,14 @@
+//while loop------------------------------>>>>
+// let i = 0;
+// while (i<=10) {
+//     console.log(`value of indexn is ${i}`);
+//     i+=2;
+// }
+
+
+//do-while--------------------------------->>>>
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
