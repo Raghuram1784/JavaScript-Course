4a9d0370be58f0d096043ca608f1819f4a30d607
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-26 05:51:09 +0000

    forEach loop & filter

---
+++ 05_iterations.js/six.js
@@ -0,0 +1,32 @@
+// const coding = ["js", "ruby", "c++", "java", "python"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+// console.log(values);
+
+
+
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = nums.filter( (num) => num>4)
+// console.log(newNums);
+
+// const newNums = nums.filter( (num) => {
+//     return num>4
+// })
+// console.log(newNums);
+
+
+
+
+
+//Using for each loop----->
+// const newNums = []
+// nums.forEach( (num) => {
+//     if(num > 4){
+//          newNums.push(num)
+//     }
+// })
+// console.log(newNums);
