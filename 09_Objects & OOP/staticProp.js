74ed85a723360ab9ec8327827606f3336ce62957
Raghuram1784 <ymvraghu1784@gmail.com>
2024-06-30 05:54:17 +0000

    Static keyword

---
+++ 09_Objects & OOP/staticProp.js
@@ -0,0 +1,27 @@
+class user{
+    constructor(userName){
+        this.userName = userName
+    }
+
+    logMe(){
+        console.log(`UserName is ${this.userName}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const ram = new user("Ram")
+// console.log(ram.createId());
+
+
+class Teacher extends user{
+    constructor(userName, email){
+        super(userName)
+        this.email = email
+    }
+}
+
+const iPhone = new Teacher("iPhone", "i@phone.com")
+console.log(iPhone.createId());
